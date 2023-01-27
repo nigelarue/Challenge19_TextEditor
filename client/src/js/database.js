7e2114c5d45f4b3e02c4aba1c4187e1b10e22aee
nigelarue <larue.nigel91@gmail.com>
2023-01-26 18:48:29 -0600

    written code for TODOs in database.js. So created methods for GET and PUT requests

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putDb implemented');
+  const jateDatabase = await openDB('jate', 1);
+  const jateText = jateDatabase.transaction('jate', 'readwrite');
+  const jateStore = jateText.objectStore('jate');
+  const jateRequest = jateStore.put({ id: 1, value: content });
+  const result = await jateRequest;
+  console.log('Transmission Received', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getDb implemented');
+  const jateDatabase = await openDB('jate', 1);
+  const jateText = jateDatabase.transaction('jate', 'readonly');
+  const jateStore = jateText.objectStore('jate');
+  const jateRequest = jateStore.get(1);
+  const result = await jateRequest;
+  console.log('Transmission Received', result.value);
+};
 
 initdb();
